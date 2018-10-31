38bd37cb1bdd0fba1ee68310349d4ca85035c7b5
Preben Huybrechts <hpreben@gmail.com>
2018-10-31 14:29:00 +0100

    Initial commit

---
+++ AzureEventhubConnectionstringTester/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AzureEventhubConnectionstringTester
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var program = new Program();
+            program.MainAsync().GetAwaiter().GetResult();
+        }
+
+        private const string ConnectionString =
+            "<your eventhub connection string>";
+
+        private const string EntityPath = "<your entitypath>";
+
+        private async Task MainAsync()
+        {
+            var builder = new EventHubsConnectionStringBuilder(ConnectionString)
+            {
+                EntityPath = EntityPath
+            };
+
+            var client = EventHubClient.CreateFromConnectionString(builder.ToString());
+
+            WriteLine("Getting partition info");
+            var info = await client.GetRuntimeInformationAsync();
+
+            foreach (var partitionId in info.PartitionIds)
+            {
+                WriteLine($"Creating receiver for partition [{partitionId}]");
+                var receiver = client.CreateReceiver(PartitionReceiver.DefaultConsumerGroupName,
+                   partitionId, EventPosition.FromStart(), new ReceiverOptions());
+
+                var messages = await receiver.ReceiveAsync(10, TimeSpan.FromMilliseconds(10));
+                WriteLine($"Got [{messages?.Count() ?? 0}] messages from partion [{partitionId}]");
+            }
+
+            Write("Finished");
+            ReadLine();
+        }
+    }
+}
